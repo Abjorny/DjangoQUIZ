d649c322b67ba4248b28e694f46e671051cad8a8
Ваше Имя <you@example.com>
2025-12-26 02:29:59 +0000

    update

--- main/Lending/static/js/sendMessage.js
+++ main/Lending/static/js/sendMessage.js
@@ -1,15 +1,18 @@
+import { getCookie } from './getCookie.js';
+
 export async function sendMessage(message, chat_id, value, name, quiz) {
     let status = true;
+
     const params = new URLSearchParams(window.location.search);
 
-    // Преобразуем null в пустые строки
     const utmData = {
-        utm_source: params.get('utm_source') || '',
-        utm_medium: params.get('utm_medium') || '',
-        utm_campaign: params.get('utm_campaign') || '',
-        utm_term: params.get('utm_term') || '',
-        utm_content: params.get('utm_content') || '',
+        utm_source: params.get('utm_source'),
+        utm_medium: params.get('utm_medium'),
+        utm_campaign: params.get('utm_campaign'),
+        utm_term: params.get('utm_term'),
+        utm_content: params.get('utm_content'),
     };
+
 
     try {
         const response = await fetch('/api/send-message/', {
@@ -24,7 +27,7 @@ export async function sendMessage(message, chat_id, value, name, quiz) {
                 value,
                 name,
                 quiz,
-                utm: utmData,
+                utm: utmData, 
             }),
         });
 
