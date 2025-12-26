992a85dbbbd75ef661c95e98cc445364355ee622
Ваше Имя <you@example.com>
2025-12-26 02:57:35 +0000

    update

--- main/ApiBot/serializers.py
+++ main/ApiBot/serializers.py
@@ -1,41 +1,16 @@
 from rest_framework import serializers
 
 class UTMSerializer(serializers.Serializer):
-    utm_source = serializers.CharField(
-        required=False, 
-        allow_blank=True, 
-        allow_null=True, 
-        default=None
-    )
-    utm_medium = serializers.CharField(
-        required=False, 
-        allow_blank=True, 
-        allow_null=True, 
-        default=None
-    )
-    utm_campaign = serializers.CharField(
-        required=False, 
-        allow_blank=True, 
-        allow_null=True, 
-        default=None
-    )
-    utm_term = serializers.CharField(
-        required=False, 
-        allow_blank=True, 
-        allow_null=True, 
-        default=None
-    )
-    utm_content = serializers.CharField(
-        required=False, 
-        allow_blank=True, 
-        allow_null=True, 
-        default=None
-    )
+    utm_source = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    utm_medium = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    utm_campaign = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    utm_term = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    utm_content = serializers.CharField(required=False, allow_blank=True, allow_null=True)
 
 class MessageSerializer(serializers.Serializer):
     chat_id = serializers.IntegerField()
     message = serializers.CharField(max_length=40960)
     value = serializers.CharField(max_length=40960)
-    name = serializers.CharField(max_length=40960)
+    name = serializers.CharField(max_length=40960, required=False, allow_blank=True)  # <-- ДОБАВЬТЕ required=False
     quiz = serializers.CharField(max_length=40960, required=False, allow_blank=True)
     utm = UTMSerializer(required=False)
